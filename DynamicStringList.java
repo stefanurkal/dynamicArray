6002415113f36608c0ce5690b0560e2b762634e1
Stefanurkal <stephenfranada@gmail.com>
2025-01-15 08:47:52 -0800

    current progress for partner

--- DynamicStringList.java
+++ DynamicStringList.java
@@ -1,12 +1,29 @@
-public class DynamicStringList {
-    int numIndex;
-    String myString;
+public class DynamicStringList implements StringList {
+    private String[] myArray;
+    
+    private int size
 
-    public DynamicStringList ( int numIndex, String myString){
-        this.numIndex = numIndex;
-        this.myString = myString; 
+    public DynamicStringList (){
+        myArray = New String[20];
+        size = 0; 
+        
     }
 
+    public String get(int numIndex){
+        return myString[numIndex];
+    }
+
+    public void set(int index, String value){
+        myString[index] = value;
+    }
+
+    public void add( String value ){
+        myString[myString.length+1] = value;  
+    }
+
+    public int size() {
+        return size;
+    }
     
 }
 
