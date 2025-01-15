f59b28c751db1f5b90eb51a487ad633dad8c3616
Stefanurkal <stephenfranada@gmail.com>
2025-01-15 12:03:47 -0800

    addition of add, size, capacity

--- DynamicStringList.java
+++ DynamicStringList.java
@@ -1,28 +1,42 @@
 public class DynamicStringList implements StringList {
     private String[] myArray;
-    
-    private int size
+    private int size;
 
     public DynamicStringList (){
-        myArray = New String[20];
+        myArray = new String[10];
         size = 0; 
         
     }
 
-    public String get(int numIndex){
-        return myString[numIndex];
+    public String get(int index){
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of range");
+        }
+        return myArray[index];
     }
 
     public void set(int index, String value){
-        myString[index] = value;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index out of range");
+        }
+        myArray[index] = value;
     }
 
     public void add( String value ){
-        myString[myString.length+1] = value;  
+
+        if (myArray.length >= size) {
+            throw new IndexOutOfBoundsException("Index out of range");
+        }
+        myArray[size] = value;
+        size++;  
     }
 
     public int size() {
         return size;
+    }
+
+    public int capacity() {
+        return myArray.length;
     }
     
 }
